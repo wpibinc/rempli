cc95ea5854384d03efce58ccbdf4de0f5398ae1b
sergey <serhii.ponomarenko@hneu.net>
2016-09-06 17:44:50 +0300

    popup

--- resources/views/where.blade.php
+++ resources/views/where.blade.php
@@ -3,7 +3,7 @@
 @section('title')
 
 @section('content')
-    <div class='reviews'>
+    <div class='reviews col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0'>
         @if(!empty($reviews))
             @foreach($reviews as $review)
             <div class="review-item">
@@ -13,21 +13,24 @@
             @endforeach
         @endif
     </div>
-    @if(Auth::user())
-        {{ Form::open(array('url' => '/reviews', 'method' => 'post')) }}
+    <div class="col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0">
+        @if(Auth::user())
+            {{ Form::open(array('url' => '/reviews', 'method' => 'post')) }}
 
-        <div class="form-group">
-            {!! Form::label('Ваш отзыв') !!}
-            {!! Form::textarea('message', null, 
-                array('required', 
-                      'class'=>'form-control', 
-                      'placeholder'=>'отзыв')) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('Ваш отзыв') !!}
+                {!! Form::textarea('message', null,
+                    array('required',
+                          'class'=>'form-control',
+                          'placeholder'=>'отзыв')) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::submit('Отправить', 
-              array('class'=>'btn btn-primary')) !!}
-        </div>
-        {{ Form::close() }}
-    @endif
+            <div class="form-group">
+                {!! Form::submit('Отправить',
+                  array('class'=>'btn btn-primary')) !!}
+            </div>
+            {{ Form::close() }}
+        @endif
+    </div>
+
 @endsection
