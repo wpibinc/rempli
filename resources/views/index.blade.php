4131aa5523021e891c2a01db60a78a93ee9ef7da
sergey <serhii.ponomarenko@hneu.net>
2016-09-16 12:05:42 +0300

    modal success

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,11 +39,31 @@
         </div>
 
     </div>
+
+    <div class="modal fade success-modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Спасибо за заказ!</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Ваш заказ был нами успешно получен. В ближайшие несколько минут, мы позвоним Вам для уточнения информации.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
         <script>
             var rubric=[];
             rubric = {!! json_encode($categories->pluck('alias')->toArray()) !!};
             var {!! implode(',', $categories->pluck('alias')->toArray()) !!} = '';
         </script>
+
+
 @endsection
 
 <script>
