de6a26ccc3a622cb54c59928652d9fdeb646880f
itbdev <itbdev1@gmail.com>
2016-10-11 13:13:22 +0300

    subscriptions fix

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -12,12 +12,35 @@ class SubscriptionController extends Controller
 {
     public function create(Request $request)
     {
-        Subscription::create($request->all());
+        $subscription = Subscription::create($request->all());
+        $subscription->start_subscription = Carbon::now();
+        $cur_date = Carbon::now();
+        $subscription->end_subscription = $cur_date->addMonths(1);
+        try {
+            $subscription->save();
+        } catch (\Exception $e) {
+            return $e;
+        }
+
         return response()->json(['status' => true]);
     }
     public function update(Request $request)
     {
-        Subscription::create($request->all());
+        $last_subscription = Subscription::where('user_id', $request->user_id)
+                ->where('is_free', '0')
+                ->orderBy('end_subscription', 'desc')->first();
+
+        $subscription = Subscription::create($request->all());
+
+        $subscription->start_subscription = $last_subscription->end_subscription;
+        $new_start_date = $last_subscription->end_subscription;
+        $subscription->end_subscription = $new_start_date->addMonths(1);
+        try {
+            $subscription->save();
+        } catch (\Exception $e) {
+            return $e;
+        }
+
         return response()->json(['status' => true]);
     }
 
@@ -33,24 +56,29 @@ class SubscriptionController extends Controller
     {
 
         $subscription = Subscription::where('promocode', $request->promocode)->first();
-        if ($subscription->start_promocode != null) {
+        if (isset($subscription)) {
+            if ($subscription->start_subscription != null) {
+                return response()->json(['status' => false]);
+            }
+            foreach ($request->all() as $key => $value) {
+                if (in_array($key, $subscription->getColumnNames())) {
+                    $subscription->{$key} = trim($value);
+                }
+            }
+
+            $subscription->start_subscription = Carbon::now();
+            $cur_date = Carbon::now();
+            $subscription->end_subscription = $cur_date->addMonths($subscription->duration);
+            try {
+                $subscription->save();
+            } catch (\Exception $e) {
+                return $e;
+            }
+            return response()->json(['status' => true]);
+        } else {
             return response()->json(['status' => false]);
         }
-        foreach ($request->all() as $key => $value) {
-            if (in_array($key, $subscription->getColumnNames())) {
-                $subscription->{$key} = trim($value);
-            }
-        }
 
-        $subscription->start_promocode = Carbon::now();
-        $cur_date = Carbon::now();
-        $subscription->end_promocode = $cur_date->addMonths($subscription->duration);
-        try {
-            $subscription->save();
-        } catch (\Exception $e) {
-            return $e;
-        }
-        return response()->json(['status' => true]);
 //        return redirect()->back()->with('success_message', 'Подписка оформлена.');
     }
 }
